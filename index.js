fc1e2138832a418a9a16aebf46f69559d68079fe
maman <mamanabdurrahman11@gmail.com>
2020-07-14 22:03:04 +0700

    update
    Signed-off-by: maman <mamanabdurrahman11@gmail.com>

--- index.js
+++ index.js
@@ -5,7 +5,10 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import bgMessaging from './src/helper/bgMessaging'; // <-- Import the file you created in (2)
 
 console.disableYellowBox = true;
 
 AppRegistry.registerComponent(appName, () => App);
+// New task registration
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => bgMessaging); // <-- Add this line
